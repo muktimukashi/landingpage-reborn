7fa4b1d74f40fbb97e4402ac5c6a89ae41d17b87
muktimukashi <budimanmukti@gmail.com>
2026-06-17 18:42:42 +0700

    Improve sticky header

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+(() => {
+  const navbar = document.querySelector(".rl-navbar");
+
+  if (!navbar) {
+    return;
+  }
+
+  const updateNavbar = () => {
+    navbar.classList.toggle("is-scrolled", window.scrollY > 8);
+  };
+
+  updateNavbar();
+  window.addEventListener("scroll", updateNavbar, { passive: true });
+})();
